9b383e2aee6766d9ba67276b6c568ac4dda75026
wongwaiwa <62935814+wongwaiwa@users.noreply.github.com>
2020-04-29 12:49:01 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,10 @@
+import discord
+from discord.est import command
+
+
+
+@bot.event
+async def on_ready[]:
+    print[">>Bot is online<<"]
+
+Bot.run["NzA0OTA2MTc1NzI4MzIwNTMy.XqkAsg.1QNZMz42FjmpnFtPk_NHb243g-s"]
